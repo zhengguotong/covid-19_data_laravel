7b543f565a901a41d022e1297b98dd1709835c17
Tony Zheng <tony@visionsoft.com.au>
2020-06-07 17:30:45 +1000

    added auth controller

--- api/config/covid19.php
+++ api/config/covid19.php
@@ -3,5 +3,5 @@
 return [
     'case_cvs_base_link' => 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
     'first_date' => '01-22-2020',
-    'colums_changed_date' => '03-21-2020',
+    'colums_changed_date' => '03-21-2020'
 ];
